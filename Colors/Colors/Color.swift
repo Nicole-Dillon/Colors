d947cac727c56a8237f58c3d6270c95ae5c450f1
Nicole Dillon <nkdvgd@mail.missouri.edu>
2019-04-07 04:37:28 -0500

    colors with navigation

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Nicole Dillon on 4/7/19.
+//  Copyright © 2019 Nicole Dillon. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
